a8bb1da6711d94fbbef58cb317721c784481c169
rolfosian <135147406+rolfosian@users.noreply.github.com>
2025-09-03 01:28:14 +1200

    optional color args for controls for VideoLibDemo console command

--- src/data/scripts/console/VideoLibDemo.java
+++ src/data/scripts/console/VideoLibDemo.java
@@ -25,6 +25,7 @@ import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.ui.UIComponentAPI;
 import com.fs.starfarer.api.ui.UIPanelAPI;
 import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.campaign.ui.UITable;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.BaseCustomUIPanelPlugin;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
@@ -63,6 +64,46 @@ public class VideoLibDemo implements BaseCommand {
             }
         }
 
+        final Color[] controlsColor = new Color[1];
+        final Color[] controlsTextColor = new Color[1];
+
+        if (splitArgs.contains("wc")) {
+            for (String arg : splitArgs) {
+                if (arg.startsWith("color:")) {
+                    String[] colorArgs = arg.split(":")[1].split(",");
+                    if (colorArgs.length > 3 || colorArgs.length < 3) throw new IllegalArgumentException("Color parameter must be \"color:R,G,B\" format format as integer values");
+
+                    for (String colorArg : colorArgs) {
+                        for (char c : colorArg.toCharArray())
+                        if (!Character.isDigit(c)) throw new IllegalArgumentException("Color parameter must be \"color:R,G,B\" format as integer values");
+                    }
+
+                    int red = Integer.parseInt(colorArgs[0]);
+                    int green = Integer.parseInt(colorArgs[1]);
+                    int blue = Integer.parseInt(colorArgs[2]);
+                    
+                    controlsColor[0] = new Color(red, green, blue);
+
+                } else if (arg.startsWith("textcolor:")) {
+                    String[] colorArgs = arg.split(":")[1].split(",");
+                    if (colorArgs.length > 3 || colorArgs.length < 3) throw new IllegalArgumentException("Text Color parameter must be \"textcolor:R,G,B\" format as integer values");
+
+                    for (String colorArg : colorArgs) {
+                        for (char c : colorArg.toCharArray())
+                        if (!Character.isDigit(c)) throw new IllegalArgumentException("Text Color parameter must be \"textcolor:R,G,B\" format as integer values");
+                    }
+
+                    int red = Integer.parseInt(colorArgs[0]);
+                    int green = Integer.parseInt(colorArgs[1]);
+                    int blue = Integer.parseInt(colorArgs[2]);
+                    
+                    controlsTextColor[0] = new Color(red, green, blue);
+                }
+            }
+
+            if (controlsTextColor[0] == null) controlsTextColor[0] = Misc.getTextColor();
+            if (controlsColor[0] == null) controlsColor[0] = Misc.getDarkPlayerColor();
+        }
 
         InteractionDialogPlugin interactionPlugin = new InteractionDialogPlugin() {
             private String currentVideoId;
@@ -70,6 +111,7 @@ public class VideoLibDemo implements BaseCommand {
             @Override
             public void init(InteractionDialogAPI dialog) {
                 CustomPanelAPI parentPanel;
+                // ***THE MEAT AND POTATOES***
 
                 // Video width and height, should be the same as the encoded video file's resolution.
                 // Width and height CAN be variable, but will incur non-negligible rescaling overhead in ffmpeg if not the same as the video's actual resolution, price especially noticeable while seeking
@@ -79,8 +121,9 @@ public class VideoLibDemo implements BaseCommand {
                 if (fileId == null) fileId = "video_lib_demo";
 
                 // with controls
-                if (splitArgs.contains("wc")) {                            // file ID defined in data/config/settings.json // starting PlayMode // starting EOFMode
-                    videoPlayer = VideoPlayerFactory.createMutePlayerWithControls(fileId, videoWidth, videoHeight, PlayMode.PAUSED, EOFMode.LOOP);
+                if (splitArgs.contains("wc")) {                    
+                                                                 // file ID defined in data/config/settings.json | starting PlayMode | starting EOFMode
+                    videoPlayer = VideoPlayerFactory.createMutePlayerWithControls(fileId, videoWidth, videoHeight, PlayMode.PAUSED, EOFMode.LOOP, controlsTextColor[0], controlsColor[0]);
                     videoPlayer.setClickToPause(true); // setClickToPause on the video so user can click it to pause/unpause it
 
                     PositionAPI masterPos = videoPlayer.getMasterPanel().getPosition();
@@ -89,9 +132,9 @@ public class VideoLibDemo implements BaseCommand {
 
                     parentPanel = showCustomPanelAndCenter(dialog, parentWidth, parentHeight);
                     videoPlayer.addTo(parentPanel).inTL(0f, 0f); // add to parent
-                    videoPlayer.init(); // init projector so it knows where/height/width to render
+                    videoPlayer.init(); // init projector and controls so they know where/height/width to render
         
-                } else {                                      // file ID defined in data/config/settings.json // starting PlayMode // starting EOFMode
+                } else {                                 // file ID defined in data/config/settings.json | starting PlayMode | starting EOFMode
                     videoPlayer = VideoPlayerFactory.createMutePlayer(fileId, videoWidth, videoHeight, PlayMode.PLAYING, EOFMode.LOOP);
                     videoPlayer.setClickToPause(true); // setClickToPause on the video so user can click it to pause/unpause it
 
@@ -100,6 +143,8 @@ public class VideoLibDemo implements BaseCommand {
                     videoPlayer.init(); // init projector so it knows where/width/height to render
                 }
 
+
+                // ignore everything below this line if you value your sanity
                 currentVideoId = fileId;
                 // May revisit this at some stage, too much debug required, repositioning is obtuse
                 if (videoPlayer instanceof MuteVideoPlayer) {
@@ -107,7 +152,7 @@ public class VideoLibDemo implements BaseCommand {
                     Color c2 = Global.getSettings().getDarkPlayerColor();
     
                     TooltipMakerAPI tt = parentPanel.createUIElement(202f, videoHeight, true);
-                    Object table = tt.beginTable(c1, c2, Misc.getHighlightedOptionColor(), 30f, false, true,
+                    UITable table = (UITable) tt.beginTable(c1, c2, Misc.getHighlightedOptionColor(), 30f, false, true,
                     new Object[]{"Available Videos by Id", 200f});
     
                     String[] videoFileIds = VideoPaths.keys();
