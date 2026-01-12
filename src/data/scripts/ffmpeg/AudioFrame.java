367feed6c468aad9bd90818517c220cee3e30a6c
rolfosian <135147406+rolfosian@users.noreply.github.com>
2026-01-13 07:02:31 +1300

    Update AudioFrame.java

--- src/data/scripts/ffmpeg/AudioFrame.java
+++ src/data/scripts/ffmpeg/AudioFrame.java
@@ -3,16 +3,10 @@ package data.scripts.ffmpeg;
 import java.nio.ByteBuffer;
 
 public class AudioFrame extends Frame {
-    public final int size;
-    public final int samplesPerChannel;
+    public final long durationUs;
 
-    public AudioFrame(ByteBuffer buffer, long pts, int size, int samplesPerChannel) {
+    public AudioFrame(ByteBuffer buffer, long pts, long durationUs) {
         super(buffer, pts);
-        this.size = size;
-        this.samplesPerChannel = samplesPerChannel;
-    }
-
-    public long durationMillis() {
-        return (samplesPerChannel * 1000L) / FFmpeg.AUDIO_SAMPLE_RATE;
+        this.durationUs = durationUs;
     }
 }
